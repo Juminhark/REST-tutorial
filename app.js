23f36d09a73f0102a3c7a52ba6aeab8c7c77d6e9
Juminhark <40381241+Juminhark@users.noreply.github.com>
2020-04-02 13:34:47 +0900

    update

--- app.js
+++ app.js
@@ -19,9 +19,15 @@ app.get('/', (req, res) => {
 //Connect To DB
 mongoose.connect(
 	process.env.DB_CONNECTION,
-	{ useNewUrlParser: true },
+	{ 
+		useNewUrlParser: true,
+		useUnifiedTopology: true 
+	},
 	() => {console.log('connected to DB!');}
 );
 		
 //How to we start listening to the server
-app.listen(3000);
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => 
+  console.log(`Server running in http://localhost:${PORT}`)
+);
